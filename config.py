340ae549f31863d170b1910bb6ed6ab6f3c3312b
lismusa-pp <lis.musa13@gmail.com>
2025-06-23 17:25:15 +0200

    Now changes for privacy

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ USGS_API_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_ho
 
 # Google Sheets
 GOOGLE_SHEET_NAME = "Earthquake Alerts"
-CREDENTIALS_FILE = "credentials.json"  # This is your service account key
-
+CREDENTIALS_FILE = "credentials.json"  # This file is in .gitignore, create your own!
 # Earthquake filtering
 MIN_MAGNITUDE = 3.5  # Only report earthquakes above this
